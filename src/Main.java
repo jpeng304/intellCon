fa00b8f18f56e4dea9ae1ae00e1517aca667633a
jpeng304 <jpeng304@hotmail.com>
2018-12-01 04:49:59 -0800

    add new print

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! ASD");
+
+        System.out.println("Hello World! As");
     }
 }
